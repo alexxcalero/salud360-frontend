fedd5491dc49d19c758421be60616802d7783879
Fabián <fabianmr2806@hotmail.com>
2025-04-29 16:04:40 -0500

    Buttons, DDLs, Inputs y Labels

--- Frontend/Salud360/src/App.tsx
+++ Frontend/Salud360/src/App.tsx
@@ -4,8 +4,17 @@ import Button from './components/Button';
 import Input from './components/Input';
 import Label from './components/Label';
 import InputLabel from './components/InputLabel';
+import Select from './components/Select';
+import SelectLabel from "./components/SelectLabel";
 
 function App() {
+
+  const optionsSelect = [
+      { value: "Hombre", content: "Hombre" },
+      { value: "Mujer", content: "Mujer" },
+      { value: "Rodrigo Roller", content: "Rodrigo Roller" }]
+  
+
   return (
     <>
       <div>
@@ -13,31 +22,42 @@ function App() {
           Proyecto React.js (o .ts XD) + TailwindCSS + Shadcn configurado!!!
           Cortesía de Fabián(U)
         </h1>
-        <div>
+
+        <div className="space-y-10">
           <div>
             <img src="https://universitario.pe/media/images/banners/PORTDA_WEB.jpg" alt="" />
           </div>
-        <Button/>
-        <Input/>
-        <Label htmlFor="email"> HOLA POBRESSSs </Label>
+          <Button/>
 
-        <div>a</div>
-        <div>a</div>
+          <Input/>
 
-        <InputLabel type="email" placeholder="email del usuario" htmlFor="email" label="contenido"/>
-        <div>a</div>
-        <div>a</div>
-        <Input/>
+          <Label htmlFor="email"> Probando Label (la línea debajo es un hr)</Label>
+
+          <hr />
+
+          <InputLabel type="email" placeholder="email del usuario" htmlFor="email" label="contenido"/>
+          
+          <Input/>
+
+          <SelectLabel options={optionsSelect} placeholder="Seleccione su genero" htmlFor="email" label="Género" />
+          
+          <NavLink
+            className="bg-gray-100 py-2 px-7 border-1 border-gray-800 rounded-xl hover:bg-gray-300 cursor-pointer"
+            to="/example"
+            end
+          >
+            Example
+          </NavLink>
+
+
         </div>
+
+
+
+
       </div>
-        
-      <NavLink
-        className="bg-gray-100 py-2 px-7 border-1 border-gray-800 rounded-xl hover:bg-gray-300 cursor-pointer"
-        to="/example"
-        end
-      >
-        Example
-      </NavLink>
+
+      
     </>
   );
 }
