22f9ef3a25278c97bb4a275e5bfe8f757856c3ef
claraaqwq <128831601+claraaqwq@users.noreply.github.com>
2025-05-02 16:44:56 -0500

    Cambios pq se caia el front

--- Backend/Salud360/src/main/java/pe/edu/pucp/salud360/usuario/models/TipoDocumento.java
+++ Backend/Salud360/src/main/java/pe/edu/pucp/salud360/usuario/models/TipoDocumento.java
@@ -8,6 +8,8 @@ import lombok.Setter;
 
 import java.time.LocalDateTime;
 import java.util.List;
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.fasterxml.jackson.annotation.JsonManagedReference;
 
 @Getter
 @Setter
--- Backend/Salud360/src/main/java/pe/edu/pucp/salud360/usuario/models/Usuario.java
+++ Backend/Salud360/src/main/java/pe/edu/pucp/salud360/usuario/models/Usuario.java
@@ -8,6 +8,8 @@ import lombok.Setter;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.fasterxml.jackson.annotation.JsonManagedReference;
 
 @Getter
 @Setter
