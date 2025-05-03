4064a06e2a365ec647637cdf1e43b28257fef5d8
valro <a20211704@pucp.edu.pe>
2025-05-02 19:42:50 -0500

    final

---
+++ Backend/Salud360/src/main/java/pe/edu/pucp/salud360/membresia/repositories/MembresiaRepository.java
@@ -0,0 +1,7 @@
+package pe.edu.pucp.salud360.membresia.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import pe.edu.pucp.salud360.membresia.models.Membresia;
+
+public interface MembresiaRepository extends JpaRepository<Membresia, Integer> {
+}
