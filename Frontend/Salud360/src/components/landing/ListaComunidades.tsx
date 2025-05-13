471f741da7af3546729e48df9511ed3ae0a6cf45
Fabián <fabianmr2806@hotmail.com>
2025-05-13 16:45:41 -0500

    Landing Page WIP 16:45 (13/05)

---
+++ Frontend/Salud360/src/components/landing/ListaComunidades.tsx
@@ -0,0 +1,28 @@
+import  { useState} from "react";
+import axios from "axios";
+
+function ListaComunidades(){
+    
+    const [comunidades, setComunidades] = useState([]);
+
+    const fetchComunidades = () => {
+    axios.get("http://localhost:8080/api/comunidades", {
+      auth: {
+        username: "admin",
+        password: "admin123"
+      }
+    })
+      .then(res => {
+        console.log("Datos cargados:", res.data); // VER ESTO EN LA CONSOLA
+        setComunidades(res.data);
+        console.log("Comunidades:", res.data);
+      })
+      .catch(err => console.error("Error cargando comunidades", err));
+    }
+
+    return (
+        <div>{comunidades}</div>
+    );
+}
+
+export default ListaComunidades;
