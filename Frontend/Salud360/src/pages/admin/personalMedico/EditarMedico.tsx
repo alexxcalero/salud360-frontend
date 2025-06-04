0c2366405ac2abcb8bf09687761fc5e465a4f884
iGorillaVR <131416317+iGorillaVR@users.noreply.github.com>
2025-06-04 11:22:04 -0500

    Validaciones Editar Usuario, Medico, Local, Servicio

--- Frontend/Salud360/src/pages/admin/personalMedico/EditarMedico.tsx
+++ Frontend/Salud360/src/pages/admin/personalMedico/EditarMedico.tsx
@@ -4,11 +4,18 @@ import { useParams } from "react-router";
 import usePersonalMedicoForm from "@/hooks/usePersonalMedicoForm";
 import PersonalMedicoForms from "@/components/admin/personalMedico/PersonalMedicoForms";
 import { useNavigate } from "react-router";
+import ModalValidacion from "@/components/ModalValidacion";
+
 
 function EditarMedico() {
   const [loading, setLoading] = useState(true);
   const { id } = useParams();
   const navigate = useNavigate();
+
+  const [showModalValidacion, setShowModalValidacion] = useState(false);
+  const [mensajeValidacion, setMensajeValidacion] = useState("");
+
+
   const {
     nombres,
     setNombres,
@@ -59,7 +66,66 @@ function EditarMedico() {
     return <p>Cargando médico...</p>; // o un spinner
   }
 
+
+  //VALIDACIONES DE CAMPOS 
+    const validarCampos = (): boolean => {
+    const soloLetras = /^[A-Za-zÁÉÍÓÚáéíóúÑñ\s]+$/;
+    const soloNumeros = /^[0-9]+$/;
+
+    if (!nombres || !soloLetras.test(nombres)) {
+      setMensajeValidacion("Los nombres deben contener solo letras y no estar vacíos.");
+      setShowModalValidacion(true);
+      return false;
+    }
+
+    if (!apellidos || !soloLetras.test(apellidos)) {
+      setMensajeValidacion("Los apellidos deben contener solo letras y no estar vacíos.");
+      setShowModalValidacion(true);
+      return false;
+    }
+
+    if (!DNI || !soloNumeros.test(DNI) || DNI.length !== 8) {
+      setMensajeValidacion("El DNI debe tener exactamente 8 dígitos numéricos.");
+      setShowModalValidacion(true);
+      return false;
+    }
+
+
+    if (!especialidad || especialidad.trim() === "") {
+      setMensajeValidacion("La especialidad no puede estar vacía.");
+      setShowModalValidacion(true);
+      return false;
+    }
+
+    if (!descripcion || descripcion.trim() === "") {
+      setMensajeValidacion("La descripción no puede estar vacía.");
+      setShowModalValidacion(true);
+      return false;
+    }
+
+    if (!tipoDoc || tipoDoc === 0) {
+      setMensajeValidacion("Debe seleccionar un tipo de documento.");
+      setShowModalValidacion(true);
+      return false;
+    }
+
+    if (!genero || genero.trim() === "") {
+      setMensajeValidacion("Debe seleccionar un género.");
+      setShowModalValidacion(true);
+      return false;
+    }
+
+    return true;
+  };
+
+
   const handleEditarMedico = async () => {
+
+    if (!validarCampos()) {
+            setShowModalValidacion(true);
+            return;
+    }
+
     try {
       const sexo = genero;
 
@@ -98,6 +164,7 @@ function EditarMedico() {
   };
 
   return (
+    <>
     <PersonalMedicoForms
       title="Editar médico"
       nombres={nombres}
@@ -118,6 +185,18 @@ function EditarMedico() {
       buttonText="Guardar"
       readOnly={false}
     />
+    {showModalValidacion && (
+                <div className="fixed inset-0 bg-black/60 z-40">
+                <div className="fixed inset-0 z-50 flex items-center justify-center">
+                    <ModalValidacion
+                    titulo="Error en los campos"
+                    mensaje={mensajeValidacion}
+                    onClose={() => setShowModalValidacion(false)}
+                    />
+                </div>
+                </div>
+            )}
+            </>
   );
 }
 
--- Frontend/Salud360/src/pages/admin/usuarios/EditarUsuario.tsx
+++ Frontend/Salud360/src/pages/admin/usuarios/EditarUsuario.tsx
@@ -4,12 +4,17 @@ import { useParams } from "react-router";
 import UsuariosForms from "@/components/admin/usuarios/UsuariosForms";
 import useUsuarioForm from "@/hooks/useUsuarioForm";
 import { useNavigate } from "react-router";
+import ModalValidacion from "@/components/ModalValidacion";
+
 
 function EditarUsuario(){
     const [loading, setLoading] = useState(true);
     const {id} = useParams();
 
     const navigate = useNavigate();
+
+    const [showModalValidacion, setShowModalValidacion] = useState(false);
+    const [mensajeValidacion, setMensajeValidacion] = useState("");
 
     const {
         nombres, setNombres,
@@ -48,7 +53,72 @@ function EditarUsuario(){
       return <p>Cargando usuario...</p>; // o un spinner
     }
 
+
+  //VALIDACIONES DE CAMPOS 
+    const validarCampos = (): boolean => {
+    const soloLetras = /^[A-Za-zÁÉÍÓÚáéíóúÑñ\s]+$/;
+    const soloNumeros = /^[0-9]+$/;
+    const regexCorreo = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+    if (!nombres || !soloLetras.test(nombres)) {
+      setMensajeValidacion("Los nombres deben contener solo letras y no estar vacíos.");
+      return false;
+    }
+
+    if (!apellidos || !soloLetras.test(apellidos)) {
+      setMensajeValidacion("Los apellidos deben contener solo letras y no estar vacíos.");
+      return false;
+    }
+
+    if (!DNI || !soloNumeros.test(DNI) || DNI.length !== 8) {
+      setMensajeValidacion("El DNI debe tener exactamente 8 dígitos numéricos.");
+      return false;
+    }
+
+    if (!telefono || !soloNumeros.test(telefono) || telefono.length !== 9) {
+      setMensajeValidacion("El teléfono debe tener exactamente 9 dígitos numéricos.");
+      return false;
+    }
+
+    if (!correo.trim() || !regexCorreo.test(correo)) {
+      setMensajeValidacion("El correo electrónico ingresado no es válido.");
+      setShowModalValidacion(true);
+      return false;
+    }
+
+    if (!direccion || direccion.trim() === "") {
+      setMensajeValidacion("La dirección no puede estar vacía.");
+      return false;
+    }
+
+    if (!contrasenha || contrasenha.trim() === "") {
+      setMensajeValidacion("La contraseña no puede estar vacía.");
+      return false;
+    }
+
+    if (!tipoDoc || tipoDoc === 0) {
+      setMensajeValidacion("Debe seleccionar un tipo de documento.");
+      setShowModalValidacion(true);
+      return false;
+    }
+
+    if (!genero || genero.trim() === "") {
+      setMensajeValidacion("Debe seleccionar un género.");
+      setShowModalValidacion(true);
+      return false;
+    }
+    return true;
+  };   
+
+
+
     const handleEditarUsuario = async() => {
+
+      if (!validarCampos()) {
+            setShowModalValidacion(true);
+            return;
+        }
+
         try{
             const sexo = genero;
 
@@ -95,6 +165,7 @@ function EditarUsuario(){
 
     
     return(
+      <>
             <UsuariosForms
                 title="Editar usuario"
                 nombres={nombres}
@@ -119,6 +190,19 @@ function EditarUsuario(){
                 buttonText="Guardar"
                 readOnly={false}
             />
+      {showModalValidacion && (
+                <div className="fixed inset-0 bg-black/60 z-40">
+                <div className="fixed inset-0 z-50 flex items-center justify-center">
+                    <ModalValidacion
+                    titulo="Error en los campos"
+                    mensaje={mensajeValidacion}
+                    onClose={() => setShowModalValidacion(false)}
+                    />
+                </div>
+                </div>
+            )}
+        </>
+        
     );
 }
 
