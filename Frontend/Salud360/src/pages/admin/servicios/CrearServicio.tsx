ada680640e9725e3496108eef840cd8c294dab86
iGorillaVR <131416317+iGorillaVR@users.noreply.github.com>
2025-06-04 02:20:56 -0500

    Validacion crear Usuario, Servicio, Medicos
    Falta sus editar

--- Frontend/Salud360/src/pages/admin/servicios/CrearServicio.tsx
+++ Frontend/Salud360/src/pages/admin/servicios/CrearServicio.tsx
@@ -1,11 +1,16 @@
 import ServiciosForm from "@/components/admin/servicios/ServiciosForm";
 import useServicioForms from "@/hooks/useServicioForms";
 import axios from "axios";
+import ModalValidacion from "@/components/ModalValidacion";
+import  { useState, useEffect } from "react";
 import { useNavigate } from "react-router";
 
 function CrearServicio(){
 
     const navigate = useNavigate();
+
+    const [showModalValidacion, setShowModalValidacion] = useState(false);
+    const [mensajeValidacion, setMensajeValidacion] = useState("");
 
     const {
         nombre, setNombre,
@@ -15,7 +20,40 @@ function CrearServicio(){
         setServicioAPI
     } = useServicioForms();
 
+
+    //VALIDACIONES DE CAMPOS 
+    const validarCampos = (): boolean => {
+    const soloLetras = /^[A-Za-zÁÉÍÓÚáéíóúÑñ\s]+$/;
+    const soloNumeros = /^[0-9]+$/;
+
+    if (!nombre || nombre.trim() === "") {
+      setMensajeValidacion("El nombre del servicio no debe estar vacíos.");
+      setShowModalValidacion(true);
+      return false;
+    }
+
+    if (!descripcion || descripcion.trim() === "") {
+      setMensajeValidacion("La descripción no puede estar vacía.");
+      setShowModalValidacion(true);
+      return false;
+    }
+
+    if (!tipo || tipo.trim() === "") {
+      setMensajeValidacion("El tipo no puede estar vacío.");
+      setShowModalValidacion(true);
+      return false;
+    }
+
+
+    return true;
+    };
+
     const handleCrearServicio = async() => {
+        if (!validarCampos()) {
+            setShowModalValidacion(true);
+            return;
+        }
+
         console.log("El contenido de los locales a enviar es:", locales)
         try{
             const response = await axios.post("http://localhost:8080/api/servicios", 
@@ -47,6 +85,7 @@ function CrearServicio(){
     }
 
     return(
+        <>
         <div className="w-full px-10 py-8 text-left">
             <ServiciosForm
                 title= "Registrar Servicio"
@@ -64,11 +103,54 @@ function CrearServicio(){
                 buttonText="Crear Servicio"
             />
         </div>
+        {showModalValidacion && (
+                <div className="fixed inset-0 bg-black/60 z-40">
+                <div className="fixed inset-0 z-50 flex items-center justify-center">
+                    <ModalValidacion
+                    titulo="Error en los campos"
+                    mensaje={mensajeValidacion}
+                    onClose={() => setShowModalValidacion(false)}
+                    />
+                </div>
+                </div>
+            )}
+            </>
     )
-
-    
-        
-
 }
 
 export default CrearServicio;
+
+/*
+const validarCampos = (): boolean => {
+        const soloLetras = /^[A-Za-zÁÉÍÓÚáéíóúÑñ\s]+$/;
+        const soloNumeros = /^[0-9]+$/;
+
+        if (!nombre || nombre.trim() === "") {
+        setMensajeValidacion("El nombre del servicio no puede estar vacíos.");
+        setShowModalValidacion(true);
+        return false;
+        }
+        if (!descripcion || descripcion.trim() === "") {
+        setMensajeValidacion("La descripción no puede estar vacía.");
+        setShowModalValidacion(true);
+        return false;
+        }
+        if (!tipo || tipo.trim() === "") {
+        setMensajeValidacion("El tipo no puede estar vacío.");
+        setShowModalValidacion(true);
+        return false;
+        }
+    }
+
+{showModalValidacion && (
+            <div className="fixed inset-0 bg-black/60 z-40">
+            <div className="fixed inset-0 z-50 flex items-center justify-center">
+            <ModalValidacion
+                titulo="Error en los campos"
+                mensaje={mensajeValidacion}
+                onClose={() => setShowModalValidacion(false)}
+            />
+            </div>
+            </div>
+         )}
+*/
--- Frontend/Salud360/src/pages/admin/usuarios/CrearUsuario.tsx
+++ Frontend/Salud360/src/pages/admin/usuarios/CrearUsuario.tsx
@@ -1,12 +1,16 @@
 import axios from "axios";
 import UsuariosForms from "@/components/admin/usuarios/UsuariosForms";
 import useUsuarioForm from "@/hooks/useUsuarioForm";
-
+import ModalValidacion from "@/components/ModalValidacion";
+import  { useState, useEffect } from "react";
 import { useNavigate } from "react-router";
 
 function CrearUsuario(){
 
     const navigate = useNavigate();
+
+    const [showModalValidacion, setShowModalValidacion] = useState(false);
+    const [mensajeValidacion, setMensajeValidacion] = useState("");
 
     const {
         nombres, setNombres,
@@ -21,7 +25,73 @@ function CrearUsuario(){
         contrasenha, setContrasenha
     } = useUsuarioForm();
 
+
+    //VALIDACIONES DE CAMPOS 
+    const validarCampos = (): boolean => {
+    const soloLetras = /^[A-Za-zÁÉÍÓÚáéíóúÑñ\s]+$/;
+    const soloNumeros = /^[0-9]+$/;
+    const regexCorreo = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+    if (!nombres || !soloLetras.test(nombres)) {
+      setMensajeValidacion("Los nombres deben contener solo letras y no estar vacíos.");
+      return false;
+    }
+
+    if (!apellidos || !soloLetras.test(apellidos)) {
+      setMensajeValidacion("Los apellidos deben contener solo letras y no estar vacíos.");
+      return false;
+    }
+
+    if (!DNI || !soloNumeros.test(DNI) || DNI.length !== 8) {
+      setMensajeValidacion("El DNI debe tener exactamente 8 dígitos numéricos.");
+      return false;
+    }
+
+    if (!telefono || !soloNumeros.test(telefono) || telefono.length !== 9) {
+      setMensajeValidacion("El teléfono debe tener exactamente 9 dígitos numéricos.");
+      return false;
+    }
+
+    if (!correo.trim() || !regexCorreo.test(correo)) {
+      setMensajeValidacion("El correo electrónico ingresado no es válido.");
+      setShowModalValidacion(true);
+      return false;
+    }
+
+    if (!direccion || direccion.trim() === "") {
+      setMensajeValidacion("La dirección no puede estar vacía.");
+      return false;
+    }
+
+    if (!contrasenha || contrasenha.trim() === "") {
+      setMensajeValidacion("La contraseña no puede estar vacía.");
+      return false;
+    }
+
+    if (!tipoDoc || tipoDoc === 0) {
+      setMensajeValidacion("Debe seleccionar un tipo de documento.");
+      setShowModalValidacion(true);
+      return false;
+    }
+
+    if (!genero || genero.trim() === "") {
+      setMensajeValidacion("Debe seleccionar un género.");
+      setShowModalValidacion(true);
+      return false;
+    }
+    return true;
+  };   
+
+
+
+
+
     const handleCrearUsuario = async() => {
+
+        if (!validarCampos()) {
+            setShowModalValidacion(true);
+            return;
+        }
         try{
             const numeroDocumento = DNI;
             const sexo = genero;
@@ -76,7 +146,7 @@ function CrearUsuario(){
     }
 
     return(
-            
+        <>
             <UsuariosForms
                 title="Registrar Usuario"
                 subtitle="Rellene los siguientes campos para completar el registro del usuario."
@@ -104,6 +174,19 @@ function CrearUsuario(){
                 buttonText="Crear Usuario"
                 readOnly={false}
             />
+            {showModalValidacion && (
+                <div className="fixed inset-0 bg-black/60 z-40">
+                <div className="fixed inset-0 z-50 flex items-center justify-center">
+                    <ModalValidacion
+                    titulo="Error en los campos"
+                    mensaje={mensajeValidacion}
+                    onClose={() => setShowModalValidacion(false)}
+                    />
+                </div>
+                </div>
+            )}
+            </>
+            
     );
     
 }
