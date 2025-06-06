2dc4e9a2ea4c06c10f0db896f7fdc53d47670012
iGorillaVR <131416317+iGorillaVR@users.noreply.github.com>
2025-06-05 23:26:08 -0500

    Update vite.config.ts

--- Frontend/Salud360/vite.config.ts
+++ Frontend/Salud360/vite.config.ts
@@ -8,17 +8,17 @@ export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      '@': path.resolve(__dirname, './src'),
     },
   },
   server: {
     host: '0.0.0.0',
     port: 5173,
+    allowedHosts: ['all'],
     strictPort: true,
     cors: true,
     hmr: {
-      clientPort: 5173
+      clientPort: 5173,
     },
-    allowedHosts: ['all'] // <- Esta línea permite cualquier dominio
-  }
+  },
 })
