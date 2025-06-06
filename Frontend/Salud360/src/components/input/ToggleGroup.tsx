95d593dd782379f149c6ca5292d3723bee62cb25
Fidnix <fidel.apari@proton.me>
2025-06-06 17:54:11 -0500

    Filtrado

---
+++ Frontend/Salud360/src/components/input/ToggleGroup.tsx
@@ -0,0 +1,52 @@
+import { cn } from "@/lib/utils";
+import { useState } from "react";
+
+interface SingleProps {
+  type: "single";
+  value?: string;
+  onValueChange?: (value: string) => void;
+  options: {
+    value: string;
+    conten: string;
+  }[];
+}
+interface MultipleProps {
+  type: "multiple";
+  value: string[];
+  onValueChange: (value: string[]) => void;
+  options: {
+    value: string;
+    content: string;
+  }[];
+}
+
+type Props = MultipleProps;
+
+const ToggleGroup = ({ type, value, onValueChange, options }: Props) => {
+  const toggleValue = (v: string) => {
+    if (value.includes(v)) {
+      onValueChange(value.filter((item) => item !== v));
+    } else {
+      onValueChange([...value, v]);
+    }
+  };
+
+  return (
+    <div className="border-neutral-400 border-1 w-max rounded-lg overflow-hidden">
+      {options.map((opt) => (
+        <button
+          key={opt.value}
+          className={cn(
+            "w-auto px-2 py-1 hover:bg-neutral-100",
+            value.includes(opt.value) && "bg-neutral-200"
+          )}
+          onClick={() => toggleValue(opt.value)}
+        >
+          {opt.content}
+        </button>
+      ))}
+    </div>
+  );
+};
+
+export default ToggleGroup;
--- Frontend/Salud360/src/components/usuario/CalendarioUsuarios.tsx
+++ Frontend/Salud360/src/components/usuario/CalendarioUsuarios.tsx
@@ -2,12 +2,31 @@ import Calendario from "../calendario/Calendario";
 import { reservaType } from "@/schemas/reserva";
 import { DateTime } from "luxon";
 import { ReservaCard } from "../calendario/reservaCard";
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import { AuthContext } from "@/hooks/AuthContext";
 import { getAllClienteReservasAPI } from "@/services/cliente.service";
+import { Switch } from "../ui/switch";
+import Select from "../Select";
+import { medicoType } from "@/schemas/medico";
+import ToggleGroup from "../input/ToggleGroup";
+import { useFetchHandler } from "@/hooks/useFetchHandler";
+import { getMedicos } from "@/services/medico.service";
 
 const CalendarioUsuarios = () => {
   const { usuario } = useContext(AuthContext);
+
+  const [medicos, setMedicos] = useState<medicoType[]>([]);
+  const [medicoSeleccionado, setMedicoSeleccionado] = useState<string>("");
+
+  const [act, setAct] = useState<string[]>(["citaMedica", "clase"]);
+  const [showCancelados, setShowCancelados] = useState(true);
+
+  const { fetch } = useFetchHandler();
+  useEffect(() => {
+    fetch(async () => {
+      setMedicos(await getMedicos());
+    });
+  }, []);
   return (
     <>
       <Calendario<reservaType>
@@ -33,6 +52,64 @@ const CalendarioUsuarios = () => {
           );
           return reservas ?? [];
         }}
+        filterFuncs={[
+          (d) => {
+            if (d.estado === "Cancelada" && !showCancelados) return false;
+
+            if (d.citaMedica && !act.includes("citaMedica")) return false;
+            if (d.clase && !act.includes("clase")) return false;
+
+            if (
+              medicoSeleccionado &&
+              d.citaMedica &&
+              d.citaMedica.medico?.idMedico?.toString() !== medicoSeleccionado
+            )
+              return false;
+
+            return true;
+          },
+        ]}
+        filterContent={
+          <div>
+            <div className="flex flex-col">
+              <strong>Mostrar canceladas</strong>
+              <Switch
+                checked={showCancelados}
+                onCheckedChange={setShowCancelados}
+              />
+            </div>
+
+            <div className="mt-4">
+              <strong>Seleccionar por medico</strong>
+              <Select
+                options={medicos.map((med) => ({
+                  value: med.idMedico?.toString() ?? "",
+                  content: `${med.nombres} ${med.apellidos} - ${med.especialidad}`,
+                }))}
+                value={medicoSeleccionado}
+                onChange={setMedicoSeleccionado}
+              />
+            </div>
+            <div className="mt-4">
+              <strong>Tipo de actividad</strong>
+              <ToggleGroup
+                type="multiple"
+                options={[
+                  {
+                    value: "citaMedica",
+                    content: "Cita medica",
+                  },
+                  {
+                    value: "clase",
+                    content: "Clase",
+                  },
+                ]}
+                value={act}
+                onValueChange={setAct}
+              />
+            </div>
+          </div>
+        }
       />
     </>
   );
