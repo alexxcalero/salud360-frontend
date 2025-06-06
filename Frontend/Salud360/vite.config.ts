06ed059217c803ff44c5783adc9778f66928b8cc
iGorillaVR <131416317+iGorillaVR@users.noreply.github.com>
2025-06-05 23:15:07 -0500

    Update vite.config.ts

--- Frontend/Salud360/vite.config.ts
+++ Frontend/Salud360/vite.config.ts
@@ -11,10 +11,13 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
-
-    server: {
-    host: '0.0.0.0',
-    port: 5173,
-    allowedHosts: 'all'
+  server: {
+    host: "0.0.0.0",        // Escucha en todas las interfaces
+    port: 5173,             // Puerto por defecto de Vite
+    strictPort: true,       // No cambia automáticamente si el puerto está ocupado
+    origin: false,          // No valida el host remoto
+    hmr: {
+      clientPort: 5173      // Hot Module Reload para clientes externos
+    }
   }
 })
