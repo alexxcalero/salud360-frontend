a92a4beea793303f596f2e33749f98c7913b1c3f
Fabián <fabianmr2806@hotmail.com>
2025-06-07 02:08:54 -0500

    Estilos Corregidos + Calendar

--- Frontend/Salud360/src/pages/usuario/CalendarioYReservas.tsx
+++ Frontend/Salud360/src/pages/usuario/CalendarioYReservas.tsx
@@ -7,14 +7,14 @@ function CalendarioYReservas() {
   }, []);
 
   return (
-    <section className="flex flex-col gap-4">
-      <div className="w-full px-8 py-8 text-left">
+    <section className="flex flex-col gap-2">
+      <div className="w-full px-8 pt-8 text-left">
         <h1 className="text-4xl font-bold mb-2">Calendario de Reservas</h1>
         <h2 className="text-lg text-gray-700 mb-6">
           En este calendario puedes visualizar, reservar y anular citas o
           actividades:
         </h2>
-        <hr className="mt-16 border" />
+        <hr className="mt-4 border" />
       </div>
 
       <div className="w-full px-8">
--- Frontend/Salud360/src/pages/usuario/DetalleComunidadHorarios.tsx
+++ Frontend/Salud360/src/pages/usuario/DetalleComunidadHorarios.tsx
@@ -4,7 +4,7 @@ import { useParams } from "react-router";
 function DetalleComunidadHorario() {
   const { id } = useParams();
   return (
-    <section>
+    <section className="w-full px-8 py-8">
       <CalendarioComunidad
         id={Number(id)}
         filtrosAdicionales={[(d) => d.estado !== "Reservada"]}
