4eae92a04ae9fc9402b4a241c4aba65d4821a799
Fidnix <fidel.apari@proton.me>
2025-06-09 12:04:55 -0500

    Docs para testing e2e

--- Frontend/Salud360/tests/auth.spec.ts
+++ Frontend/Salud360/tests/auth.spec.ts
@@ -1,6 +1,9 @@
 import { test, expect } from "@playwright/test";
 import { faker } from "@faker-js/faker";
 
+/**
+ * Guía primaria en `./landing.spec.ts`
+ */
 test.describe("Testing para formularios", () => {
   test.beforeEach(async ({ page }) => {
     await page.goto("http://localhost:5173/");
@@ -9,10 +12,13 @@ test.describe("Testing para formularios", () => {
   test("Registro", async ({ page }) => {
     const registrarBtn = page.getByRole("link", { name: "Registrate" });
     await expect(registrarBtn).toBeVisible();
+    // Se hace click en el botón de registrar
     await registrarBtn.click();
 
+    // Se verifica que exista el título h1 de registro
     await expect(page.getByRole("heading", { name: "REGISTRO" })).toBeVisible();
 
+    // Se declaran variables para manejar el formulario
     const nombresInput = page.locator('[name="nombres"]');
     const apellidosInput = page.locator('[name="apellidos"]');
     const fechaNacimiento = page.locator('[name="fechaNacimiento"]');
@@ -26,7 +32,13 @@ test.describe("Testing para formularios", () => {
     const contrasenia = page.locator('[name="contraseña"]');
     const confirmarContrasenia = page.locator('[name="confirmarContraseña"]');
 
+    // Se crea data falsa con faker.js (módulo aparte de playwright)
     const generoOpciones = faker.helpers.arrayElement(["male", "female"]);
+
+    // Se escribe data falsa en los inputs
+    // Y como son acciones, se tiene que esperar a que acabe con await
+    // Es muy importante await aquí pq no usarlo, haría que se escriba el texto en otro input
+    // Por desincronización
     await nombresInput.fill(faker.person.firstName(generoOpciones));
     await apellidosInput.fill(faker.person.lastName(generoOpciones));
     await fechaNacimiento.fill(
@@ -55,41 +67,65 @@ test.describe("Testing para formularios", () => {
     await contrasenia.fill(contraFakeData);
     await confirmarContrasenia.fill(contraFakeData);
 
+    // Permite tomar screenshots dentro de los resultados
+    // Reescribe un screenshot si tiene el mismo nombre
+    await page.screenshot({
+      path: "test-image-results/registro.jpg", // Se guarda desde la carpeta src/,
+      // pero especifiquen siempre que se guarde en test-image-results, pq no lo considerara git por .gitignore
+      fullPage: true, // fullPage toma captura de toda la pantalla, pero puede tener problemas con el elemento sticky,
+      // ya que playwright tmb scrollea
+    });
+
+    // Se clickea en el botón de registrar (No con google)
     const submitBtn = page.getByRole("button", {
       name: "Registrarse",
       exact: true,
     });
     await submitBtn.click();
 
+    // Y se espera que uno se encuentre en la ventana de felicidades por el registro
     await expect(
       page.getByRole("heading", { name: "¡Felicidades!" })
     ).toBeVisible();
   });
 
   test("Login usuario", async ({ page }) => {
+    // Se hace click en el botón de inciar sesión
+    // Se obtiene el iniciar sesión por regexp
     const loginBtn = page.getByRole("link", { name: /Iniciar Sesi[oó]n/ });
-    await expect(loginBtn).toBeVisible();
+    await expect(loginBtn).toBeVisible(); // Esto no es obligatorio siempre, solo es mi manía
     await loginBtn.click();
 
+    // Se espera estar en la página correcta. También se puede usar
+    // > await expect(page).toHaveTitle(...)
     await expect(
       page.getByRole("heading", { name: "INICIAR SESIÓN" })
     ).toBeVisible();
 
+    // Declara y rellena los inputs de formulario
     const correo = page.locator('[name="correo"]');
     const contrasenia = page.locator('[name="contraseña"]');
 
     await correo.fill("usuario@hotmail.com");
     await contrasenia.fill("usuario123");
 
+    await page.screenshot({
+      path: "test-image-results/login_usuario.jpg",
+      fullPage: true,
+    });
+
+    // Hace click en iniciar sesión
     const submitBtn = page.getByRole("button", {
       name: "Iniciar Sesión",
       exact: true,
     });
     await submitBtn.click();
 
+    // Se espera que la bienvenida tenga el nombre del usuario
     await expect(page).toHaveTitle(/Bienvenido/);
   });
 
+  // Lo mismo que el anterior, pero para admin
   test("Login admin", async ({ page }) => {
     const loginBtn = page.getByRole("link", { name: /Iniciar Sesi[oó]n/ });
     await expect(loginBtn).toBeVisible();
@@ -104,6 +140,11 @@ test.describe("Testing para formularios", () => {
 
     await correo.fill("fabianmr2806@hotmail.com");
     await contrasenia.fill("admin123");
+
+    await page.screenshot({
+      path: "test-image-results/login_admin.jpg",
+      fullPage: true,
+    });
 
     const submitBtn = page.getByRole("button", {
       name: "Iniciar Sesión",
