61fad42e6754532399983fb239891142775840b4
iGorillaVR <131416317+iGorillaVR@users.noreply.github.com>
2025-06-14 18:33:48 -0500

    Logica para descarga de archivos usuario Admin Front

--- Frontend/Salud360/src/components/ModalValidacion.tsx
+++ Frontend/Salud360/src/components/ModalValidacion.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { createPortal } from "react-dom";
 
 interface Props {
   titulo?: string;
@@ -8,9 +9,45 @@ interface Props {
 }
 
 function ModalValidacion({ titulo = "Validación", mensaje = "", textoBoton = "Entendido", onClose }: Props) {
-  return (
+  return createPortal(
+    //PARA QUE NO SE CLICKEEN COSAS DEL FONDO DENTRO DEL FORM
+    <div
+      className="fixed inset-0 z-[9999] bg-black/60 flex items-center justify-center"
+      style={{ pointerEvents: "auto" }}
+    >
+      <div
+        className="bg-white p-8 rounded-2xl shadow-md max-w-md w-full text-center z-[10000]"
+        onClick={(e) => e.stopPropagation()}
+      >
+        {/* Ícono informativo */}
+        <div className="flex justify-center mb-4">
+          <div className="bg-yellow-400 rounded-full w-16 h-16 flex items-center justify-center">
+            <span className="text-white text-3xl font-bold">!</span>
+          </div>
+        </div>
+
+        <h2 className="text-2xl font-bold mb-2">{titulo}</h2>
+        <p className="mb-6">{mensaje}</p>
+
+        <button
+          className="bg-blue-500 hover:bg-blue-600 text-white font-semibold px-6 py-2 rounded"
+          onClick={onClose}
+        >
+          {textoBoton}
+        </button>
+      </div>
+    </div>,
+    document.body
+  );
+}
+
+export default ModalValidacion;
+
+//Version sin portal pq se loquea con el form de reserva:
+/*
+return (
     <div className="bg-white p-8 rounded-2xl shadow-md max-w-md w-full text-center">
-      {/* Ícono informativo */}
+      {/Ícono informativo}
       <div className="flex justify-center mb-4">
         <div className="bg-yellow-400 rounded-full w-16 h-16 flex items-center justify-center">
           <span className="text-white text-3xl font-bold">!</span>
@@ -28,6 +65,5 @@ function ModalValidacion({ titulo = "Validación", mensaje = "", textoBoton = "E
       </button>
     </div>
   );
-}
 
-export default ModalValidacion;
+*/
