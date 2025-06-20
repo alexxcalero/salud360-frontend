9c1d6b2d779196e4f52e48cfcec8c79b5d04040c
Fidnix <fidel.apari@proton.me>
2025-06-19 23:01:54 -0500

    Correccion de metodo de pago

--- Frontend/Salud360/src/components/usuario/membresia/SeleccionarMetodo.tsx
+++ Frontend/Salud360/src/components/usuario/membresia/SeleccionarMetodo.tsx
@@ -14,6 +14,7 @@ import { IComunidad } from "@/models/comunidad";
 import { IMembresia } from "@/models/membresia";
 import { getAllMediosDePagosByUsuarioAPI } from "@/services/medioDePago.service";
 import { AuthContext } from "@/hooks/AuthContext";
+import { IMedioDePago } from "@/models/medioDePago";
 
 const SeleccionarMetodo = ({
   comunidad,
