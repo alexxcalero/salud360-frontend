6caad81044a819259215ffd9ebde5888967f485e
Fidnix <fidel.apari@proton.me>
2025-07-07 11:26:38 -0500

    Estrellitas :D

--- Frontend/Salud360/src/components/modals/ModalTestimonio.tsx
+++ Frontend/Salud360/src/components/modals/ModalTestimonio.tsx
@@ -3,6 +3,7 @@ import Button from "@/components/Button";
 import useTestimonioForm from "@/hooks/useTestimonioForm";
 import { useEffect, useState } from "react";
 import ErrorModal from "@/components/modals/errorModal";
+import { Star } from "lucide-react";
 
 interface Props {
   onClose: () => void;
@@ -38,6 +39,15 @@ function ModalTestimonio({ onClose, onSubmit, defaultValues}: Props) {
           <p className="text-gray-600">Comparte tu experiencia con la comunidad</p>
 
           <div className="flex flex-col gap-4">
+            <label className="text-sm font-semibold">Calificación</label>
+            <div className="flex justify-center items-center gap-1">
+              {Array.from({ length: 5 }, (_, i) => (
+                <button onClick={() => setCalificacion(i + 1)} key={i} className="p-1">
+                  <Star size={38} className={calificacion >= i + 1 ?  "text-amber-300" : "text-neutral-500"}/>
+                </button>
+              ))}
+            </div>
+
             <InputLabel
               type="textarea"
               label="Comentario"
@@ -50,17 +60,6 @@ function ModalTestimonio({ onClose, onSubmit, defaultValues}: Props) {
             {mensajeValidacion && (
               <p className="text-red-500 text-sm -mt-2">{mensajeValidacion}</p>
             )}
-
-            <label className="text-sm font-semibold">Calificación</label>
-            <select
-              value={calificacion}
-              onChange={(e) => setCalificacion(Number(e.target.value))}
-              className="border p-2 rounded w-full"
-            >
-              {[1, 2, 3, 4, 5].map((n) => (
-                <option key={n}>{n}</option>
-              ))}
-            </select>
           </div>
 
           <div className="flex justify-end gap-4 mt-4">
